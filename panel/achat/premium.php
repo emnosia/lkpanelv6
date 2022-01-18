21b6891d2e5d12cdb3271e03ebc7a146bc034537
emnosia <65652478+emnosia@users.noreply.github.com>
2022-01-19 00:15:37 +0100

    Add files via upload

---
+++ panel/achat/premium.php
@@ -0,0 +1,122 @@
+<?php
+header('Location: https://lkpanel.me/panel/premium.php');
+
+include '../core/class/include.php';
+
+if (Account::isAuthentified())
+{
+
+
+if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+}
+
+$webhook = "WEBHOOK HERE "; //<------------     WEBHOOK HERE    
+$timezonehere = "Europe";
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "Unknown OS Platform";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'KaliLinux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+    }   
+    return $os_platform;
+}
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+							'/Mozilla/i'	=>	'Mozila',
+							'/Mozilla/5.0/i'=>	'Mozila',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+							'/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+							'/Bot/i'		=>	'BOT Browser',
+							'/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'Handheld Browser'
+                        );
+    foreach ($browser_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+    }
+    return $browser;
+}
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+
+$pseudo = $_SESSION["discord_socket"]->username;
+$pseudo2 = $_SESSION["discord_socket"]->discriminator;
+$iddis = $_SESSION["discord_socket"]->id;
+$id = Account::GetUser($_SESSION['id'])['id'];
+$rank = Account::GetUser($_SESSION['id'])['rank'];
+$ban = Account::GetUser($_SESSION['id'])['banned'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$site_refer = $_SERVER['HTTP_REFERER'];
+	if($site_refer == ""){
+		$site = "dirrect connection";
+	}
+else{
+		$site = $site_refer;
+	}
+date_default_timezone_set('$timezonehere');
+$time = date('Y-m-d');
+$heur = date('H:i:s');
+$make_json = json_encode(array ('content'=>"
+<@$iddis> à payer le grade VIP !
+
+Discord ID: **$iddis**
+Panel ID: **$id**
+Panel Rank: **$rank**
+
+Date: **$time**
+Heur: **$heur\n**
+
+<@688430744963514394> & <@676727512394629130>
+"));
+$exec = curl_init(""); 
+curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $exec, CURLOPT_POST, 1);
+curl_setopt( $exec, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $exec, CURLOPT_HEADER, 0);
+curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $exec );
+
+}
